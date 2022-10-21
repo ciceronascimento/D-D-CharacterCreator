81693f2eb00097e1fa0e069eb75727b99832c416
Cicero Nascimento <antoniociceero@gmail.com>
2022-10-21 09:57:06 -0300

    rename project

--- DnDCharacterCreator/Controller/CharacterSheetController.swift
+++ DnDCharacterCreator/Controller/CharacterSheetController.swift
@@ -44,11 +44,21 @@ class CharacterSheetController: UIViewController, UITextFieldDelegate {
         return table
     }()
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         tableView.dataSource = self
         tableView.register(FormCell.self, forCellReuseIdentifier: "form_cell")
         tableView.delegate = self
+
         view.addSubview(textField)
         view.addSubview(tableView)
         view.addSubview(labelIncoming)
